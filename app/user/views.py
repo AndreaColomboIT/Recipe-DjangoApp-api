4e9054a526bf6092111435af448c83e7dfc1a23a
AndreaColomboIT <androcolo8@gmail.com>
2023-09-23 17:19:13 +0200

    Added: User API Calls and methods

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework import generics , authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (UserSerializer,AuthTokenSerializer)
+
+
+class CreateUserView(generics.CreateAPIView):
+    # Create a new user in the system
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    # Create a new auth token for user
+    serializer_class = AuthTokenSerializer
+    # uses default renderer classes
+    renderer_classe = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Override get object
+    def get_object(self):
+        """Retrieve and return the authenticated user"""
+        return self.request.user
